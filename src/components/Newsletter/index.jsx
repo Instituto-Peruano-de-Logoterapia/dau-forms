c1e84c3775688635dee582851d65c7408f214528
PieroDev23 <pierodavilaaguirre22@gmail.com>
2024-07-29 20:54:36 -0500

    newsletter section implemented

--- src/components/Newsletter/index.jsx
+++ src/components/Newsletter/index.jsx
@@ -1,25 +1,53 @@
 'use client'
 
-import React, { useRef, useState } from 'react'
 import { roboto } from '@app/fonts';
+import { useState } from 'react';
 import { Form } from '../Form';
 
-import { FormControl, Input, FormLabel, Flex, Button } from '@chakra-ui/react'
+import { Button, Flex, FormControl, Input } from '@chakra-ui/react';
 
 export function Newsletter() {
+
+    const [formState, setStateForm] = useState({
+        email: {
+            value: '',
+            isInvalid: false
+        },
+        phone: {
+            value: '',
+            isInvalid: false
+        }
+    });
+
+
+    const onInputChange = (event) => {
+        setStateForm(prev => ({
+            ...prev,
+            [event.target.name]: {
+                value: event.target.value,
+                isInvalid: false
+            }
+        }));
+    }
 
     const onSubmit = (event) => {
         event.preventDefault();
 
-        const email = inputEmailRef.current.value;
-
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const { email, phone } = formState;
 
-        if (!email.length || !emailRegex.test(email)) {
+        if (!emailRegex.test(email) || email.length === 0) {
+            setStateForm(prev => ({ ...prev, email: { ...prev.email, isInvalid: true } }))
             return;
         }
 
-        const phone = inputPhoneRef.current.value;
+        if (phone.length === 0) {
+            setHasErrors(prev => ({ ...prev, phone: { ...prev.phone, isInvalid: true } }));
+            return;
+        }
+
+        console.log({ formState });
+
     }
 
 
@@ -34,21 +62,25 @@ export function Newsletter() {
                     <Flex gap={5} alignItems={'flex-end'} justifyContent={'center'} className='w-11/12 m-auto flex-col md:flex-row'>
                         <FormControl isRequired>
                             <Input
+                                isInvalid={formState.phone.isInvalid}
                                 color={'black'}
-                                ref={inputPhoneRef}
                                 bg={'white'}
+                                name='phone'
                                 borderRadius={0}
                                 placeholder='Ingresa tu número de teléfono'
+                                onChange={onInputChange}
                             />
                         </FormControl>
                         <FormControl isRequired>
                             <Input
+                                isInvalid={formState.email.isInvalid}
                                 color={'black'}
-                                ref={inputEmailRef}
                                 bg={'white'}
                                 borderRadius={0}
                                 type='email'
+                                name='email'
                                 placeholder='Ingresa tu correo aquí'
+                                onChange={onInputChange}
                             />
                         </FormControl>
                         <Button
